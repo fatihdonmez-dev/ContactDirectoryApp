98c850e54fa6f48514ba5f248fc41c332c719443
Fatih Dönmez <fatih__donmez@hotmail.com>
2023-10-17 23:43:38 +0300

    added ef dbcontext for migration

---
+++ Person.API/Models/PersonDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Person.API.Models
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
